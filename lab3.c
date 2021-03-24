31639e429995e43ad884f97c38f20280e67fc418
svtrev <revtsova_st@edu.surgu.ru>
2021-03-24 16:27:45 +0500

    for

---
+++ lab3.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+
+{
+    float f, h, x;
+    printf("Введите шаг h=");
+    scanf("%f", &h);
+    printf("\tx\t f(x)\n");
+    printf("__________________________\n");
+    for(x=0; x<1.6; x+=h)
+    {
+        f=exp(sin(2*x));
+        printf("%8.3f\t %8.4f\n", x, f);
+    }
+    return 0;
+}
