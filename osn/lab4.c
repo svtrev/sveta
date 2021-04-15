38e580c74fdcc905bec0bac2d005304ae105af89
user <user@ws1013402128.pm>
2021-04-15 16:43:59 +0500

    -

--- osn/lab4.c
+++ osn/lab4.c
@@ -1,43 +1,44 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-/*Вычисление интеграла с помощью метода средних прямоугольников */
-double integral (int n) {
-   int i;
-   double x, h, integ = 0;
-	h = (M_PI)/n;
+double integral(int n)
+{
+    int i;
+    double x, h, integ = 0;
+    h = (M_PI) / n;
 
-	for (i = 0; i <= n; i++) {
-		x = -M_PI_2 +  i*h + (h/2);
+    for (i = 0; i <= n; i++)
+    {
+        x = -M_PI_2 + i * h + (h / 2);
 
-		if (x <= 0)
-			integ += ((x*x) - 2*(x*x*x)) * cos(x*x);
-		else if (x <= M_PI_2)
-			integ += exp(sin(2*x));
-	}
+        if (x <= 0)
+            integ += ((x * x) - 2 * (x * x * x)) * cos(x * x);
+        else if (x <= M_PI_2)
+            integ += exp(sin(2 * x));
+    }
 
-   return integ*h;
+    return integ * h;
 }
 
-int main() {
-	
+int main()
+{
+    double e, k = 1.0, s = 0.0, d = 1.0;
+    int n = 1;
 
-	double I, e;
-	int n = 1;
+    printf("Введите требуемую точность e = ");
+    scanf("%lf", &e);
 
+    while (d >= e)
+    {
+        d = (fabs(k - s)) / 3;
+        s = k;
+        n *= 2;
+        k = integral(2 * n);
+        printf("%lf\t%lf\t%d\n", k, d, n);
+    }
 
-	/* Вычисляем погрешность по правилу Рунге */
-	printf ("Введите требуемую точность e = ");
-	scanf("%lf", &e);
+    printf("Значение интеграла I = %.5lf\n", k);
 
-	while((fabs(integral(2*n) - integral(n)))/3 >= e) {
-		/*d = (fabs(integral(2*n) - integral(n)))/3; */
-		n *= 2;
-	}
-	I = integral(n);
-
-	printf("Значение интеграла I = %.5lf\n", I);
-
-	return 0;
+    return 0;
 }
