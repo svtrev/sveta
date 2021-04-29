edf84e717dd1e447c2e7c42147cd35fba0b6edc6
user <user@ws1013402128.pm>
2021-04-29 12:59:51 +0500

    hate

--- osn/lab6_1.c
+++ osn/lab6_1.c
@@ -1,54 +1,70 @@
 #include <math.h>
-#include <stdlib.h>
 #include <stdio.h>
-
-int main() {
-
-	int i, j, n, A[20];
-	int temp, k, m = 0, count;
-
-	printf("Введите кол-во элементов = ");
-	scanf("%d", &n);
-
-	for (i = 0; i < n; i++) {
-		A[i] = rand()%51-25;
-		printf("%d ", A[i]);
-	}
-	printf("\n");
-
-	printf("Введите искомое число = ");
-	scanf("%d", &k);
-
-	for (i = 0; i < n; i++) {
-		if (A[i] == k) break;
-		m++;
-	}
-
-	if (m != 0)
+#include <stdlib.h>
+# include <time.h>
+void fill( int n , int a[])
+{
+		int i;
+    for (i = 0; i < n; i++)
     {
-		    for (i = 0; i < m/2; i++) {
-			  temp = A[i];
-			  A[i] = A[m-1 - i];
-			  A[m-1 - i] = temp;
-		}
-		count = (n-m)/2;
-		for (i = m+1, j = 0; count > 0; count--, j++) {
-			temp = A[i+j];
-			A[i+j] = A[n-1 - j];
-			A[n-1 - j] = temp;
-		}
-	}
+        a[i] = rand() % 51 - 25;
+    }
+}
+int main()
+{
+    int i, n, t, k, j, m = 0, c;
+		srand(time(NULL));
+    printf("Vvedite kolvo elementov = ");
+    scanf("%d", &n);
+		int A[n];
 
-	if (m == 0) {
-		for (i = 0; i < (n / 2); i++) {
-			temp = A[i];
-			A[i] = A[n-1 - i];
-			A[n-1 - i] = temp;
-		}
-	}
+    fill(n, A);
 
-	for (i = 0; i < n; i++)
-		printf("%d ", A[i]);
-printf("\n");
-return 0;
+		for (i = 0; i < n; i++)
+    {
+        printf("%5d ", A[i]);
+    }
+    printf("\n");
+
+		printf("Vvedite iskomoe chislo = ");
+    scanf("%d", &k);
+
+    for (i = 0; i < n; i++)
+    {
+        if (A[i] == k)
+            break;
+        m++;
+    }
+
+    if (m != 0)
+    {
+        for (i = 0; i < m / 2; i++)
+        {
+            t = A[i];
+            A[i] = A[m - 1 - i];
+            A[m - 1 - i] = t;
+        }
+        c = (n - m) / 2;
+        for (i = m + 1, j = 0; c > 0; c--, j++)
+        {
+            t = A[i + j];
+            A[i + j] = A[n - 1 - j];
+            A[n - 1 - j] = t;
+        }
+    }
+
+    if (m == 0)
+    {
+        for (i = 0; i < (n / 2); i++)
+        {
+            t = A[i];
+            A[i] = A[n - 1 - i];
+            A[n - 1 - i] = t;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+        printf("%5d ", A[i]);
+    printf("\n");
+    return 0;
 }
